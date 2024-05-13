7a4d0349a1497850b45b4ecdfd1f6d0a84978c5f
jordanoboyle <jmcmcode@gmail.com>
2024-05-13 13:32:54 -0600

    Rendered individual data from contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,18 @@
 class ContactsController < ApplicationController
   def show_info
+
+   
+    # show some data from the database
+    contact = Contact.find_by(id: 4)
     render json: {
-      message: "Hello there"
+      id: contact.id,
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_num: contact.phone_num
+
     }
+
+    # rebuild to render all contacts
   end
 end
