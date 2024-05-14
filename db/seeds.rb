ca517737ac40d09baf3a317ad7b2ee48aac4e16e
jordanoboyle <jmcmcode@gmail.com>
2024-05-14 11:44:36 -0600

    added seed code, for database generation. Added faker gem for productivity purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+
+# 20.times do
+#   contact = Contact.new(first_name: "Bob", last_name: "Mayer", email: "bobby@school.com", phone_num: "9238593845")
+#   contact.save  
+#   contact = Contact.new(first_name: "bill", last_name: "kirk", email: "billybob@school.com", phone_num: "92fsdgdfg3845")
+#   contact.save
+#   contact = Contact.new(first_name: "Jenny", last_name: "James", email: "james@school.com", phone_num: "9fsaf93845")
+#   contact.save
+# end
